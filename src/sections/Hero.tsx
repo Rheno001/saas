9a82d98fb16c436fbfad457ae40bb8cf3b7f993d
Rheno001 <renfredreuben@gmail.com>
2025-01-16 15:26:53 +0100

    update

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -11,7 +11,8 @@ export const Hero = () => {
       <div className='md:w-[478px]'>
         <div className="text-[14px] inline-flex border border-[#222]/10 px-[12px] py-[4px] rounded-lg tracking-tight">Version 2.0 is here</div>
         <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">Pathway to Productivity</h1>
-        <p className="text-[20px] text-[#010d3e] tracking-tight mt-6">Celebrate the joy of an accomplishment with an app design to track your progress, mtivate your efforts and celebrate your successes.</p>
+        <p className="text-[20px] text-[#010d3e] tracking-tight mt-6">
+          Celebrate the joy of an accomplishment with an app design to track your progress, motivate your efforts and celebrate your successes.</p>
         <div className="flex gap-1 items-center mt-[30px] ">
           <button className="btn btn-primary">Get for Free</button>
           <button className="btn btn-text gap-1"><span>Learn More</span><ArrowIcon className='h-5 w-5'/></button>
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,28 @@
+import acmeLogo from '@/assets/logo-acme.png';
+import quantomLogo from '@/assets/logo-quantum.png';
+import echo from '@/assets/logo-echo.png';
+import celestialLogo from '@/assets/logo-celestial.png';
+import pulseLogo from '@/assets/logo-pulse.png';
+import apexLogo from '@/assets/logo-apex.png';
+import Image from 'next/image';
+
+
+
 export const LogoTicker = () => {
-  return null;
+  return <div className='py-[32px] bg-white'>
+    <div className='container'>
+      <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
+      <div className="flex gap-[56px] flex-none">
+        <Image src={acmeLogo} alt='acme logo' className='logo-ticker-image'/>
+        <Image src={quantomLogo} alt='quantum logo' className='logo-ticker-image'/>
+        <Image src={echo} alt='echo logo' className='logo-ticker-image'/>
+        <Image src={celestialLogo} alt='celestial logo' className='logo-ticker-image'/>
+        <Image src={apexLogo} alt='apex logo' className='logo-ticker-image'/>
+        <Image src={pulseLogo} alt='pulse logo' className='logo-ticker-image'/>
+      </div>
+      </div>
+
+    </div>
+
+  </div>;
 };
