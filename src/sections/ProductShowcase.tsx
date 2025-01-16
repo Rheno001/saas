1617c33f161f991c1ea161796c5070b3ba31cc6a
Rheno001 <renfredreuben@gmail.com>
2025-01-16 22:12:52 +0100

    update

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -10,13 +10,13 @@ export const ProductShowcase = () => {
         <div className='flex justify-center'>
           <div className='tag'>Boost your Productivity</div>
         </div>
-        <h2 className='text-center text-3xl md:text-[54px] md;leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-5'>A more effective way to track progress</h2>
-        <p className='text-center text-[22px] leading-[30px] tracking-tight text-[#010d3e] mt-5'>Effortlessly turn your ideas into a fully functional, responsive, Saas Website in just minutes with this template.</p>
+        <h2 className='section-title mt-5'>A more effective way to track progress</h2>
+        <p className='section-description mt-5'>Effortlessly turn your ideas into a fully functional, responsive, Saas Website in just minutes with this template.</p>
       </div>
       <div className='relative'>
        <Image src={productImage} alt='productImage' className='mt-[40px]'/>
        <Image src={pyramidImage} alt='pyramidiMAGE' className='hidden md:block absolute -right-36 -top-32' height={262} width={262}/>
-       <Image src={tubeImage} alt='tubeImage' height={248} className='hidden md:block hidden md:blockabsolute bottom-24 -left-36'/>
+       <Image src={tubeImage} alt='tubeImage' height={248} className='hidden md:block absolute bottom-24 -left-36'/>
       </div>
     </div>
   </section>;
