422fbf5638b714e06c4305e132b1d52565e453ee
Rheno001 <renfredreuben@gmail.com>
2025-01-16 15:48:35 +0100

    update

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -9,7 +9,7 @@ export const ProductShowcase = () => {
       </div>
       <h2 className='text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text'>A more effective way to track progress</h2>
       <p className='text-center'>Effortlessly turn your ideas into a fully functional, responsive, Saas Website in just minutes with this template.</p>
-      <Image src={productImage} alt='productImage' className='' />
+      <Image src={productImage} alt='productImage' />
     </div>
   </section>;
 };
