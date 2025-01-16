89c72846acd1c4a79d09f03f6c2a63beaac8cc1f
Rheno001 <renfredreuben@gmail.com>
2025-01-16 01:06:35 +0100

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import {Header} from "@/sections/Header";
+import {Hero} from '@/sections/hero';
+
+export default function Home() {
+  return(
+    <>
+      <Header/>
+      <Hero/>
+    </>
+  );
+}
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,16 @@
+export const Hero = () => {
+  return <section>
+    <div className="container">
+      <div>
+        <div className="text-[14px] inline-flex border border-[#222]/10 px-[12px] py-[]">Version 2.0 is here</div>
+        <h1>Pathway to Productivity</h1>
+        <p>Celebrate the joy of an accomplishment with an app design to track your progress, mtivate your efforts and celebrate your successes.</p>
+        <div>
+          <button>Get for Free</button>
+          <button>Learn More</button>
+        </div>
+      </div>
+
+    </div>
+  </section>;
+};
